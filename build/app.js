0dbd502c34d9c1bf9f0620231ef9fa7cd29c480b
nasonkin-aa <game-akaynt@mail.ru>
2022-05-18 17:53:06 +1000

    Delite changes

--- build/app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import router from './router/index';
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static('./public'));
-app.use(router);
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-});
