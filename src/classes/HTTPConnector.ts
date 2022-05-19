c62ad7d590017e66d77a805511a94b7dbc98b0c8
nasonkin-aa <game-akaynt@mail.ru>
2022-05-19 17:19:31 +1000

    Add HTTP connection

---
+++ src/classes/HTTPConnector.ts
@@ -0,0 +1,21 @@
+import net from 'net';
+import axios from 'axios';
+
+export default class HTTPConnector {
+  instance = axios;
+
+  ip = '';
+
+  port = 0;
+
+  constructor(ip: string, port: number) {
+    this.ip = ip;
+    this.port = port;
+
+    this.instance.create({
+      baseURL: `http://${ip}:${port}`
+    });
+  }
+
+  connect() {}
+}
