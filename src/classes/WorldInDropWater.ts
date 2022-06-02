07d538d7048127073481f786d1a09b54d8e36fd6
nasonkin-aa <game-akaynt@mail.ru>
2022-06-02 12:44:15 +1000

    Add new exposition

---
+++ src/classes/WorldInDropWater.ts
@@ -0,0 +1,27 @@
+import { IEquipment, TEquipment } from '../types/index';
+import HTTPConnector from './HTTPConnector';
+
+export default class AquaStadium extends HTTPConnector implements IEquipment {
+  name: string;
+
+  id: number;
+
+  type: number;
+
+  on() {
+    console.log('on blob');
+    return this.instance.post('/setMainCtrl.cgi?Type=9&SubType=274&Value=1')
+  }
+
+  off() {
+    console.log('off blob');
+    return this.instance.post('/setMainCtrl.cgi?Type=9&SubType=274&Value=0')
+  }
+
+  constructor(eq: TEquipment) {
+    super(eq.ip, eq.port);
+    this.name = eq.name;
+    this.id = eq.id;
+    this.type = eq.type;
+  }
+}
